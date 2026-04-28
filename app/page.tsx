10f5b73e90fd49aa5b1abc284b199fc7c6c5e715
Nikita <belka.nik02@gmail.com>
2026-04-28 20:02:35 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,5 @@
 import RootRedirect from '@/components/RootRedirect/RootRedirect';
-import { headers } from 'next/headers';
 
-export default async function RootPage() {
-  const headersList = await headers();
-  const acceptLanguage = headersList.get('accept-language') || '';
-  const lang = acceptLanguage.toLowerCase();
-
-  const defaultLang = lang.includes('en') ? 'en' : 'ua';
-
-  return <RootRedirect defaultLang={defaultLang} />;
+export default function RootPage() {
+  return <RootRedirect defaultLang='ua' />;
 }
